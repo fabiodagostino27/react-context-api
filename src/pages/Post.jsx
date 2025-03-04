64a95e6318e43338de22426f4ad151e905f91ccf
fabiodagostino27 <fabiodago27@gmail.com>
2025-03-04 12:55:55 +0100

    ADD: dynamic button

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,11 +4,14 @@ import { GlobalContext } from "../contexts/GlobalContext";
 
 export default function Post() {
     const {id} = useParams();
-    const {post, fetchPost} = useContext(GlobalContext);
+    const {post, fetchPost, posts, fetchPosts} = useContext(GlobalContext);
 
     const navigate = useNavigate();
     useEffect(() => fetchPost(id), [id]);
+    useEffect(fetchPosts, [])
 
+    const lastPost = (posts) ? posts[posts.length - 1] : "";
+ 
     return (
         <div className="container">
             <h2>{post.title}</h2>
@@ -21,7 +24,11 @@ export default function Post() {
             </p>
             <div className="d-flex gap-2 justify-content-center">
                 <button className={`btn btn-primary ${(id == 1) ? "disabled" : "d-block"}`} onClick={() => navigate(`/posts/${parseInt(id) - 1}`)}>Vai al post precedente</button>
-                <button className={`btn btn-primary ${(id == 5) ? "disabled" : "d-block"}`} onClick={() => navigate(`/posts/${parseInt(id) + 1}`)}>Vai al post successivo</button>
+                <button 
+                    className={`btn btn-primary ${lastPost && (id == lastPost.id) ? "disabled" : ""}`} 
+                    onClick={() => navigate(`/posts/${parseInt(id) + 1}`)}>
+                        Vai al post successivo
+                </button>
             </div>
         </div>
     )
